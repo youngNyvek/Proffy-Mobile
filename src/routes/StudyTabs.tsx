dcc90d25f981f408e613f349f097348e6dfa6c7f
Kevin Faria <kevintxd73@gmail.com>
2020-08-08 17:02:26 -0300

    Backend conectado
    foi utilizado o Axios para conectar o backend com o frontend da aplicação

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -34,7 +34,7 @@ function StudyTabs(){
                 },
                 inactiveBackgroundColor: '#fafafc',
                 activeBackgroundColor: '#ebebf5',
-                inactiveTintColor: "c1bccc",
+                inactiveTintColor: '#c1bccc',
                 activeTintColor: '#32264d'
             }}
         

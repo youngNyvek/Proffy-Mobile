4534ff2141f0cd283fe5adda23b56b72fac67a09
Kevin Faria <kevintxd73@gmail.com>
2020-08-07 21:12:28 -0300

    Todas as paginações concluidas

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { View } from 'react-native'
 import styles from './styles'
 
-
+import PageHeader from '../../components/PageHeader'
 
 function TeacherList(){
-    return <View style={styles.container}></View>
+    return (
+    <View style={styles.container}>
+        <PageHeader title="Proffys disponiveis"/>
+    </View>
+    )
 }
 
 
